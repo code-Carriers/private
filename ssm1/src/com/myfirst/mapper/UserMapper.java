54ac782878ba114f9d78b4a08f6c0fed78b81a3b
lu007 <1273795211@qq.com>
2019-02-20 14:27:37 +0800

    delete ssm1 dir

--- ssm1/src/com/myfirst/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.myfirst.mapper;
-
-import com.myfirst.pojo.User;
-
-public interface UserMapper {
-	public void register(User user); 
-      
-    public User login(User user);
-}
--- ssm1/src/com/myfirst/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.myfirst.service;
-
-import com.myfirst.pojo.User;
-
-public interface UserService {
-	public void register(User user); 
-    
-    public User login(User user);
-}
